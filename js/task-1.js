9a3632084854c38ed7affc819f47e3aae1c42cdc
Alina Komerysta <alinakomerista3@gmail.com>
2024-12-07 06:13:26 +0100

    commit

--- js/task-1.js
+++ js/task-1.js
@@ -5,19 +5,19 @@ const customer = {
   orders: ["Burger", "Pizza", "Salad"],
   // Change code below this line
   getBalance() {
-    return `${this.balance}`;
+    return this.balance;
   },
   getDiscount() {
-    return `${this.discount}`;
+    return this.discount;
   },
   setDiscount(value) {
     this.discount = value;
   },
   getOrders() {
-    return `${this.orders}`;
+    return this.orders;
   },
   addOrder(cost, order) {
-    this.balance -= cost - cost * `${this.discount}`;
+    this.balance -= cost - cost * this.discount;
     this.orders.push(order);
   },
   // Change code above this line
